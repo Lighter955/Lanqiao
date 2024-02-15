5dbe9da86480fb12dfc4bfff3f938976b3a249a3
Lighter955 <root@lightdev.top>
2024-02-15 20:32:41 +0800

    添加 gitignore

--- src/最少砝码.java
+++ src/最少砝码.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 
 public class 最少砝码 {
 
 	public static void main(String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		scan.close();
 	}
 
 }
