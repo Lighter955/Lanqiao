43baa49c32568cd920132956e66e2b3f31e46fc4
Lighter955 <root@lightdev.top>
2024-02-16 15:34:13 +0800

    合法日期 ✓

--- src/week02/合法日期.java
+++ src/week02/合法日期.java
@@ -1,5 +1,33 @@
 package week02;
 
-public class 合法日期 {
+import java.util.Scanner;
 
+public class 合法日期 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int d = scan.nextInt();
+        scan.close();
+        if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
+            if (d > 31) {
+                System.out.println("no");
+            } else {
+                System.out.println("yes");
+            }
+        } else if (m == 4 || m == 6 || m == 9 || m == 11) {
+            if (d > 30) {
+                System.out.println("no");
+            } else {
+                System.out.println("yes");
+            }
+        } else if (m == 2) {
+            if (d > 28) {
+                System.out.println("no");
+            } else {
+                System.out.println("yes");
+            }
+        } else {
+            System.out.println("no");
+        }
+    }
 }
