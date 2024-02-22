e273647dd34b99e9db6738553811436465bce3b5
Lighter955 <root@lightdev.top>
2024-02-22 22:36:13 +0800

    求和 ✓

---
+++ src/week02/求和.java
@@ -0,0 +1,16 @@
+package week02;
+
+import java.util.Scanner;
+
+public class 求和 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int n = 1; n <= 2019; n++) {
+            String str = Integer.toString(n);
+            if (str.contains("2") | str.contains("0") | str.contains("1") | str.contains("9")) {
+                sum += n;
+            }
+        }
+        System.out.println(sum);
+    }
+}
