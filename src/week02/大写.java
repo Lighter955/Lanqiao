e1233270fe2720244af1979c28c6897612459d08
Lighter955 <root@lightdev.top>
2024-02-22 21:55:40 +0800

    大写 ✓

---
+++ src/week02/大写.java
@@ -0,0 +1,13 @@
+package week02;
+
+import java.util.Scanner;
+
+public class 大写 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        scan.close();
+        str = str.toUpperCase();
+        System.out.println(str);
+    }
+}
