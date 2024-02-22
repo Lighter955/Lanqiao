1ce8bfe6f3bc374a7712248c6c5c81f4b3634742
Lighter955 <root@lightdev.top>
2024-02-22 22:40:18 +0800

    天数 ✓

---
+++ src/week02/天数.java
@@ -0,0 +1,18 @@
+package week02;
+
+import java.util.Scanner;
+
+public class 天数 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        scan.close();
+        if (m == 2) {
+            System.out.println(28);
+        } else if (m == 4 || m == 6 || m == 9 || m == 11) {
+            System.out.println(30);
+        } else {
+            System.out.println(31);
+        }
+    }
+}
