82e94dc9e4fbc2b288fb52205275c5a32cfc1b40
Lighter955 <root@lightdev.top>
2024-02-23 20:00:42 +0800

    明明的随机数 ✓

---
+++ src/week02/明明的随机数.java
@@ -0,0 +1,22 @@
+package week02;
+
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class 明明的随机数 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        Set<Integer> set = new TreeSet<>();
+        for (int i = 0; i < N; i++) {
+            set.add(scan.nextInt());
+        }
+        scan.close();
+        System.out.println(set.size());
+        for (Integer n : set) {
+            System.out.printf("%d ", n);
+        }
+    }
+}
