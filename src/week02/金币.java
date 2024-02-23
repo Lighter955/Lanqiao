b79f28c9f043e87d6dd27e0adcac24d6dfcb935e
Lighter955 <root@lightdev.top>
2024-02-23 14:39:59 +0800

    金币 ✓

---
+++ src/week02/金币.java
@@ -0,0 +1,21 @@
+package week02;
+
+import java.util.Scanner;
+
+public class 金币 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int k = scan.nextInt();
+        scan.close();
+        int day = 0;
+        int i = 0;
+        int sum = 0;
+        while (day < k) {
+            i++;
+            day += i;
+            sum += i * i;
+        }
+        sum -= (day - k) * i;
+        System.out.println(sum);
+    }
+}
