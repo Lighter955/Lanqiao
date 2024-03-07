e67b1228fb68e0757ac3a7c4f73bf4c9e3b6aca6
Lighter955 <root@lightdev.top>
2024-03-07 17:35:42 +0800

    二零二三 ✓

---
+++ src/week03/day01/二零二三.java
@@ -0,0 +1,23 @@
+package week03.day01;
+
+public class 二零二三 {
+    public static void main(String[] args) {
+        /*int sum = 0;
+        char[] flags = {'2', '0', '2', '3'};
+        for (int num = 12345678; num < 98765432; num++) {
+            String string = Integer.toString(num);
+            int i  = 0, j = 0;
+            while (i < 8 && j < 4) {
+                if (string.charAt(i) == flags[j]) {
+                    j += 1;
+                }
+                i += 1;
+            }
+            if (j == 4) {
+                sum += 1;
+            }
+        }
+        System.out.println(98765432 - 12345678 - sum + 1);*/
+        System.out.println(85959030);
+    }
+}
